b3e7ce472b7c7dfcd435295761345fc0e214e962
rahulforallp <rahul.kumar@knoldus.in>
2016-11-07 11:23:21 +0530

    streaming improved

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -3,7 +3,10 @@ package controllers
 import java.io.File
 import javax.inject._
 
+import akka.stream.scaladsl.{FileIO, Source}
+import akka.util.ByteString
 import play.api.Play
+import play.api.http.HttpEntity
 import play.api.mvc._
 
 
@@ -15,9 +18,18 @@ class HomeController @Inject()(webJarAssets: WebJarAssets) extends Controller {
   }
 
   def streaming = Action {
-    Ok.sendFile(
+    val file = new java.io.File("conf/vid2.mp4")
+    val path: java.nio.file.Path = file.toPath
+    val source: Source[ByteString, _] = FileIO.fromPath(path)
+
+    Result(
+      header = ResponseHeader(200, Map.empty),
+      body = HttpEntity.Streamed(source, Some(file.length), Some("application/mp4"))
+    )
+
+    /*Ok.sendFile(
       content = new File("conf/vid2.mp4"),
       inline = true
-    )
+    )*/
   }
 }
