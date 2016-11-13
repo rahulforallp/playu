19177931fc13b8d2a7f3035a764531cb139924c5
rahulforallp <rahul.kumar@knoldus.in>
2016-11-13 17:09:04 +0530

    preload added

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,11 +1,9 @@
 package controllers
 
-import java.io.File
 import javax.inject._
 
 import akka.stream.scaladsl.{FileIO, Source}
 import akka.util.ByteString
-import play.api.Play
 import play.api.http.HttpEntity
 import play.api.mvc._
 
@@ -13,23 +11,27 @@ import play.api.mvc._
 @Singleton
 class HomeController @Inject()(webJarAssets: WebJarAssets) extends Controller {
 
-  def index = Action {
-    Ok(views.html.index("Your new application is ready.", webJarAssets))
+  def index = {
+    Action {
+      Ok(views.html.index("Your new application is ready.", webJarAssets))
+    }
   }
 
-  def streaming = Action {
-    val file = new java.io.File("conf/vid2.mp4")
-    val path: java.nio.file.Path = file.toPath
-    val source: Source[ByteString, _] = FileIO.fromPath(path)
+  def streaming = {
+    Action {
+      val file = new java.io.File("conf/vid2.mp4")
+      val path: java.nio.file.Path = file.toPath
+      val source: Source[ByteString, _] = FileIO.fromPath(path)
 
-    Result(
-      header = ResponseHeader(200, Map.empty),
-      body = HttpEntity.Streamed(source, Some(file.length), Some("application/mp4"))
-    )
+      Result(
+        header = ResponseHeader(200, Map.empty),
+        body = HttpEntity.Streamed(source, Some(file.length), Some("application/mp4"))
+      )
 
-    /*Ok.sendFile(
+      /*Ok.sendFile(
       content = new File("conf/vid2.mp4"),
       inline = true
     )*/
+    }
   }
 }
